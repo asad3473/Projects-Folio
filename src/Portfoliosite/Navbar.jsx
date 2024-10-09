a6865f4a03026805444ff4fe4c87b9ae1757598f
asad3473 <asadsoomro0311@gmail.com>
2024-10-09 10:07:56 +0500

    Your commit message

--- src/Portfoliosite/Navbar.jsx
+++ src/Portfoliosite/Navbar.jsx
@@ -25,7 +25,7 @@ export default function Navbar() {
           <div className='icon absolute z-50 right-0 sm:top-4 top-4 pe-2    md:hidden' onClick={toggleNavbar}>
              { show ? <HiMiniXMark/>:<FaBars/>} 
           </div>
-          <ul className={`flex justify-around absolute bg-[#6b8ac83a]  z-10 gap-4 flex-col top-0 lg:flex-row   w-full md:static md:w-max md:bg-transparent left-0 items-center duration-300  ${show? ' ': 'top-[-100vh] md:flex md:flex-row'} `}>
+          <ul className={`flex justify-around absolute bg-[#6b8ac83a]  z-10 gap-4 flex-col top-0 lg:flex-row   w-full md:static md:w-max md:bg-transparent left-0 items-center duration-500  ${show? ' ': 'top-[-100vh] md:flex md:flex-row'} `}>
             {/* Main navbar items */}
             <li className='px-7 py-0'>
               <a href="#img" onClick={()=>setShow(false)} className='anchor'>Home</a>
